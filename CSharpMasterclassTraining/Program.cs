ba2ffd8833928dd59e5cef3740d965785aaf3dd8
Nipuna Sudharaka <nipuna.14@cse.mrt.ac.lk>
2018-11-11 05:53:08 +0530

    Initialize with hello world sample

---
+++ CSharpMasterclassTraining/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpMasterclassTraining
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Defyn!");
+        }
+    }
+}
